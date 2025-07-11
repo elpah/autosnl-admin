b56bd6283d18850f13f48527460dc75c6b6642c6
Elpah <obengelpachris@gmail.com>
2025-07-12 01:04:39 +0200

    created new routes for getting cars

--- Backend/src/services/carServices.js
+++ Backend/src/services/carServices.js
@@ -23,4 +23,85 @@ const addNewCar = async (carData) => {
   }
 };
 
-export { addNewCar };
+const getAllCars = async (page = 1) => {
+  const limit = 30;
+  const db = await connectToDatabase();
+  const carCol = db.collection("cars");
+  const dealerCol = db.collection("dealers");
+
+  const skip = (page - 1) * limit;
+
+  const projection = {
+    _id:0,
+    carId: 1,
+    carImages: 1,
+    "lang.en.carBrand": 1,
+    "lang.en.carModel": 1,
+    dealer: 1,
+    price_incl_btw: 1,
+  };
+
+  const totalCars = await carCol.countDocuments();
+  const cars = await carCol
+    .find({}, { projection })
+    .skip(skip)
+    .limit(limit)
+    .toArray();
+
+  const dealerIds = [
+    ...new Set(cars.map((car) => car.dealer).filter(Boolean)),
+  ];
+  
+  const dealers = await dealerCol
+    .find({ dealerId: { $in: dealerIds } })
+    .project({ dealerId: 1, dealerName: 1 })
+    .toArray();
+
+  const dealerMap = Object.fromEntries(
+    dealers.map((dealer) => [dealer.dealerId, dealer.dealerName])
+  );
+
+  const updatedCars = cars.map((car) => ({
+    ...car,
+    carImages:[car.carImages[0]],
+    dealer: dealerMap[car.dealer] || null,
+  }));
+
+  return { totalCars, cars: updatedCars };
+};
+
+
+const getCarById = async (id) => {
+  try {
+    const db = await connectToDatabase();
+    const carCol = db.collection("cars");
+    const dealerCol = db.collection("dealers");
+
+
+    const car = await carCol.findOne({ carId: id }, { projection :{_id:0}});
+    if(car && car.dealer)
+    {
+      const dealerInfo = await dealerCol.findOne(
+        { dealerId: car.dealer },
+        {
+          projection: {
+            dealerName: 1,
+            dealerEmail: 1,
+            dealerAddress: 1,
+            dealerPhone: 1,
+            _id: 0,
+          },
+        }
+      );
+      car.dealer = dealerInfo
+    }
+
+    return car;
+  } catch (err) {
+    console.error("Error in getCarById:", err.message);
+    return null; 
+  }
+};
+
+
+export { addNewCar, getAllCars, getCarById };

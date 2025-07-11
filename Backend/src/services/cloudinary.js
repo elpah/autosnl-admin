35aa5e6934a97d4f6a7782875968e81a51bf617d
Elpah <obengelpachris@gmail.com>
2025-07-11 13:16:31 +0200

    add missing package

--- Backend/src/services/cloudinary.js
+++ Backend/src/services/cloudinary.js
@@ -1,7 +1,6 @@
 import cloudinary from "cloudinary";
 import { Readable } from "stream";
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,

9a50173e4234721f2a91dc42c011f32739de85a8
Elpah <obengelpachris@gmail.com>
2025-07-24 03:17:12 +0200

    added not found page and redirect to dashboard

--- Frontend/src/pages/notFoundPage/NotFound.tsx
+++ Frontend/src/pages/notFoundPage/NotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./notfound.module.css";
+
 const NotFound = () => {
   return (
-	<div>Page Not Found!!</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 - Page Not Found</h1>
+      <p className={styles.message}>
+        Sorry, the page you’re looking for doesn’t exist.
+      </p>
+      <Link to="/dashboard" className={styles.homeLink}>
+        Back To Dashboard
+      </Link>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
